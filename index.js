a3eb7f7d49aa3bb290dc5d4b0d38022e6bc1ca01
jean-benoit daille <68898672+jeanbenoitdaille@users.noreply.github.com>
2021-12-06 13:25:43 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function createIdentifier(length) {
+    let result = "";
+    let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (let i = 0; i < length; i++) {
+       result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+ }
+ 
+ console.log(createIdentifier(15));
